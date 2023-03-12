0708f663f82002471201830f6bd84e55ce3c7c29
vvcb <vvcb.n1@gmail.com>
2023-03-12 20:03:42 +0000

    Mental - but kind of works - see details below
    Ended up talking to @manics (how frigging cool is that) -> https://discourse.jupyter.org/t/after-oauthenticator-successfully-returned-the-user-kubespawner-stuck-at-creating-pvc/17056
    
    Turns out there is a problem with OAuthenticator not working with http_proxy. However, the opposite true in my case.
    
    AAD authentication only works when `https_proxy` is set. However, when this env var is set,. subsequent pod deployment fails. I suspsect I have not configured the `no_proxy` variable correctly - but that is for another day.
    
    So, this is what this commit does:
    
    - Replace the `oauth2.py` file in oauthenticator with a custom file. In this do the following:
      - Set `os.environ["https_proxy"] = "http://lthswproxy01.xlthtr.nhs.uk:800"`
      - `AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")` to make use of proxies.
      - Once authentication is successful, set `os.environ["https_proxy"] =""`
      - Needs tidying up and checking if this works for multiple users and what happens in _race_ conditions when more than one user is trying to log in at the same time.
      -

--- helm_deploy_jupyterhub_private.sh
+++ helm_deploy_jupyterhub_private.sh
@@ -38,10 +38,16 @@ helm upgrade \
     --values ./helm_chart_values/singleuser.yaml \
     --values ./helm_chart_values/users.yaml \
     --values ./helm_chart_values/workspaces.yaml \
-    --set-file hub.extraFiles.customConfig.stringData=./config/jupyterhub_config_custom.py \
     --set-file hub.extraFiles.customPageTemplate.stringData=./templates/custom_page.html \
     --set-file hub.extraFiles.customSpawnPageTemplate.stringData=./templates/custom_spawn.html \
-    --set-file hub.extraFiles.customLogo.binaryData=./templates/lander_logo.png.b64
+    --set-file hub.extraFiles.customLogo.binaryData=./templates/lander_logo.png.b64 \
+    --set-file hub.extraFiles.customOauthenticator.stringData=./hacks/oauthenticator_oauth2.py 
+
+    # --set-file hub.extraFiles.customTornadoHttpClient.stringData=./hacks/tornado_httpclient.py \
+
+    # --set-file hub.extraFiles.customConfig.stringData=./config/jupyterhub_config_custom.py \
+    # --set-file hub.extraFiles.customAioConnector.stringData=./hacks/aiohttp_connector.py
+    # --set-file hub.extraFiles.customK8sRestClient.stringData=./hacks/kubernetes_asyncio___client__rest.py \
 #====================================================================
 kubectl config use-context $CURRENTAKSCONTEXT
 #====================================================================
